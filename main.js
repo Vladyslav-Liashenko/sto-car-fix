d056b142bd0270ba471043ae227027331e1d2266
Vlad Liashenko <107635535+Vladyslav-Liashenko@users.noreply.github.com>
2023-04-01 15:23:55 +0300

    save

---
+++ main.js
@@ -0,0 +1,24 @@
+// mobile-menu
+// (() => {
+//     const menuContainer = document.querySelector('.js-menu-container');
+//     const openMenuBtn = document.querySelector('.js-open-menu');
+//     const closeMenuBtn = document.querySelector('.js-close-menu');
+    
+//     const updateicon = document.querySelector(".img-header-update");
+
+  
+//     const toggleMenu = () => menuContainer.classList.toggle('is-open');
+//     openMenuBtn.addEventListener('click', toggleMenu);
+//     closeMenuBtn.addEventListener('click', toggleMenu);
+//     updateicon.setAttribute('xlink:href', './images/close-svgrepo-com.svg')
+  
+//     // Закрываем мобильное меню на более широких экранах
+//     // в случае изменения ориентации устройства.
+//     window
+//       .matchMedia('(min-width: 600px)')
+//       .addEventListener(
+//         'change',
+//         e => e.matches && menuContainer.classList.remove('is-open'),
+//       );
+// })();
+//   
