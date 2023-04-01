e98cfc5063210c5d230ee5249731e1c6c5123fea
Vlad Liashenko <107635535+Vladyslav-Liashenko@users.noreply.github.com>
2023-04-01 17:03:11 +0300

    save header mobile

--- main.js
+++ main.js
@@ -1,24 +1,23 @@
 // mobile-menu
-// (() => {
-//     const menuContainer = document.querySelector('.js-menu-container');
-//     const openMenuBtn = document.querySelector('.js-open-menu');
-//     const closeMenuBtn = document.querySelector('.js-close-menu');
-    
-//     const updateicon = document.querySelector(".img-header-update");
+(() => {
+    const menuContainer = document.querySelector('.js-menu-container');
+    const openMenuBtn = document.querySelector('.js-open-menu');
+    const closeMenuBtn = document.querySelector('.js-close-menu');
+    const updateicon = document.querySelector(".img-header-update");
 
   
-//     const toggleMenu = () => menuContainer.classList.toggle('is-open');
-//     openMenuBtn.addEventListener('click', toggleMenu);
-//     closeMenuBtn.addEventListener('click', toggleMenu);
-//     updateicon.setAttribute('xlink:href', './images/close-svgrepo-com.svg')
+    const toggleMenu = () => menuContainer.classList.toggle('is-open');
+    openMenuBtn.addEventListener('click', toggleMenu);
+    closeMenuBtn.addEventListener('click', toggleMenu);
+    
   
-//     // Закрываем мобильное меню на более широких экранах
-//     // в случае изменения ориентации устройства.
-//     window
-//       .matchMedia('(min-width: 600px)')
-//       .addEventListener(
-//         'change',
-//         e => e.matches && menuContainer.classList.remove('is-open'),
-//       );
-// })();
+    // Закрываем мобильное меню на более широких экранах
+    // в случае изменения ориентации устройства.
+    window
+      .matchMedia('(min-width: 677px)')
+      .addEventListener(
+        'change',
+        e => e.matches && menuContainer.classList.remove('is-open'),
+      );
+})();
 //   
